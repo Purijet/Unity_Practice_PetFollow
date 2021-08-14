4d50833bef884e5aa1be82f00a6e218bb6ced7fd
Purijet <86511482+Purijet@users.noreply.github.com>
2021-08-14 12:58:50 +0800

    0814Homework
    給寵物新增script，可以位移跟隨主人但無法旋轉

---
+++ UnityPetFollow/Assets/Scripts/PetFollow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PetFollow : MonoBehaviour
+{
+    [Header("速度"), Range(0, 1000)]
+    public float speed = 1.5f;
+
+    /// <summary>
+    /// 要追蹤的玩家
+    /// </summary>
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.Find("野蠻人").transform;
+    }
+
+    void Update()
+    {
+        Track();
+    }
+
+    private void Track()
+    {
+        Vector3 posTarget = target.position;
+        Vector3 posPet = transform.position;
+        Vector3 targetRelativeDirection = target.position - transform.position;
+
+        if (targetRelativeDirection.magnitude > 3)
+        {
+            transform.position = Vector3.Lerp(posPet, posTarget, 0.5f * Time.deltaTime * speed);
+        }
+    }
